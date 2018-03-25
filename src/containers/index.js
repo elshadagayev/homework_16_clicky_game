4eaf128d6803c50b86c2144593829795e207137a
Elshad Agayev <elshad@cryptomessage.io>
2018-03-25 06:51:40 -0700

    commit

--- src/containers/index.js
+++
@@ -1,5 +0,0 @@
-import HomePage from './HomePageContainer'
-import ScoreHistory from './ScoreHistoryContainer'
-import ScoreBoard from './ScoreBoardContainer'
-
-export { HomePage, ScoreHistory, ScoreBoard }
